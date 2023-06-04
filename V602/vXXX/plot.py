2f8a6dcdb0e97e1b39be002d91ccc337addbf124
Svenja Dreyer <svenja.dreyer@tu-dortmund.de>
2023-06-05 01:28:43 +0200

    Auswertung 2/3 fertig

--- V602/vXXX/plot.py
+++ V602/vXXX/plot.py
@@ -63,12 +63,43 @@ plt.clf()
 
 ta = ufloat(22.5,0.1)
 tb = ufloat(20.2,0.1)
+h = 6.626e-34
+c = 2.997e8
+d = 201.4e-12
+e_0 = 1.602e-19
 
 def Bragg(t):
-    d = 201.4e-12
     return 2*d*unp.sin(t)
 
-print('lamda für Kalpha: ',Bragg(ta*np.pi/180))
+#print('lamda für Kalpha: ',Bragg(ta*np.pi/180))
+#print('lamda für Kbeta: ',Bragg(tb*np.pi/180))
+
+def E(t):
+    return h*c/(2*d*unp.sin(t*np.pi/180)*e_0)
+
+#print('E_kalpha = ',E(ta) )
+#print('E_kbeta = ',E(tb) )
+ta_1 = ufloat(22.25,0.1)
+ta_2 = ufloat(22.7,0.1)
+
+tb_1 = ufloat(20,0.1)
+tb_2 = ufloat(20.4,0.1)
+
+print('deltaE_kalpha = ',E(ta_2)-E(ta_1) )
+print('deltaE_kbeta = ',E(tb_2)-E(tb_1))
+
+print('A_Ka = ',E(ta)/abs((E(ta_2)-E(ta_1))))
+print('A_Kb = ',E(tb)/abs((E(tb_2)-E(tb_1))) )
+
+E_abs = 8980.476
+R = 13.6
+
+s1 = 29 - unp.sqrt(E_abs/R)
+print("s1: " , s1)
+s2 = 29 - 2* unp.sqrt((E_abs - E(ta))/R)
+print("s2: " , s2)
+s3 = 29 - 3* unp.sqrt((E_abs - E(tb))/R)
+print("s3: " , s3)
 ###### Absorptionsspektrum ######
 #### Brom Br ####
 
