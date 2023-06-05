48b276b9a255ef6fa8708ad6db952dfa90f58eb9
Svenja Dreyer <svenja.dreyer@tu-dortmund.de>
2023-06-05 19:00:24 +0200

    Auswertung fertig

--- V602/vXXX/plot.py
+++ V602/vXXX/plot.py
@@ -103,7 +103,7 @@ s3 = 29 - 3* unp.sqrt((E_abs - E(tb))/R)
 ###### Absorptionsspektrum ######
 #### Brom Br ####
 
-print('----- Brom -------')
+#print('----- Brom -------')
 datenBr = np.loadtxt('AbsorberBr.csv', delimiter=',', skiprows=1)
 
 theta_br = datenBr[:,0]
@@ -113,9 +113,9 @@ I_maxBr = np.max(I_Br)
 I_minBr = np.min(I_Br)
 I_kBr = np.min(I_Br)+(np.max(I_Br)-np.min(I_Br))/2
 
-print('I_minBr = ',np.min(I_Br))
-print('I_maxBr = ',np.max(I_Br))
-print('I_kBr = ',I_kBr)
+#print('I_minBr = ',np.min(I_Br))
+#print('I_maxBr = ',np.max(I_Br))
+#print('I_kBr = ',I_kBr)
 
 def sigmoid(x, a, b):
     return a*x+b 
@@ -132,8 +132,10 @@ for name, value, uncertainty in zip('ab', params, uncertainties):
 
 t_br = (22.5 +904)/35
 t_br1 = ufloat(26.47, 0.05)
-print('t_br =', t_br)
-print('E_kBr = ', E(t_br1/2))
+#print('t_br =', t_br)
+#print('E_kBr = ', E(t_br1/2))
+E_kBr =  E(t_br1/2)
+
 plt.plot(np.linspace(26.4,26.6,50),sigmoid(np.linspace(26.4, 26.6, 50), 35, -904), color ='green', label=r'Hilfsgerade')
 plt.plot(t_br, I_kBr, 'rx', label=r'$I_K$')
 plt.plot(theta_br, I_Br, 'bx', label='Messwerte')
@@ -146,7 +148,7 @@ plt.savefig('build/AbsorberBr.pdf')
 plt.clf()
 
 ##### Gallium Ga #####
-print('--------- Gallium --------------')
+#print('--------- Gallium --------------')
 datenGa = np.loadtxt('AbsorberGa.csv', delimiter=',', skiprows=1)
 
 theta = datenGa[:,0]
@@ -156,9 +158,9 @@ I_maxGa = np.max(I_ga)
 I_minGa = np.min(I_ga)
 I_kGa = np.min(I_ga)+(np.max(I_ga)-np.min(I_ga))/2
 
-print('I_minGa = ',np.min(I_ga))
-print('I_maxGa = ',np.max(I_ga))
-print('I_kGa = ',I_kGa)
+#print('I_minGa = ',np.min(I_ga))
+#print('I_maxGa = ',np.max(I_ga))
+#print('I_kGa = ',I_kGa)
 
 x_ga = [34.4, 34.5]
 y_ga = [50, 60 ]
@@ -173,8 +175,9 @@ for name, value, uncertainty in zip('ab', params, uncertainties):
 
 t_ga = (54.5 + 3390)/100
 t_ga1 = ufloat(t_ga, 0.05)
-print('t_ga =', t_ga)
-print('E_kGa = ', E(t_ga1/2))
+#print('t_ga =', t_ga)
+#print('E_kGa = ', E(t_ga1/2))
+E_kGa =  E(t_ga1/2)
 
 plt.plot(np.linspace(34.4,34.5,50),sigmoid(np.linspace(34.4, 34.5, 50), 100, -3390), color ='green', label=r'Hilfsgerade')
 plt.plot(t_ga, I_kGa, 'rx', label=r'$I_K$')
@@ -188,7 +191,7 @@ plt.savefig('build/AbsorberGa.pdf')
 plt.clf()
 
 #### Zink Zn ######
-print('--------- Zink---------------')
+#print('--------- Zink---------------')
 datenZn = np.loadtxt('AbsorberZn.csv', delimiter=',', skiprows=1)
 
 theta = datenZn[:,0]
@@ -198,9 +201,9 @@ I_maxZn = np.max(I_Zn)
 I_minZn = np.min(I_Zn)
 I_kZn = np.min(I_Zn)+(np.max(I_Zn)-np.min(I_Zn))/2
 
-print('I_minZn = ',np.min(I_Zn))
-print('I_maxZn = ',np.max(I_Zn))
-print('I_kZn = ',I_kZn)
+#print('I_minZn = ',np.min(I_Zn))
+#print('I_maxZn = ',np.max(I_Zn))
+#print('I_kZn = ',I_kZn)
 
 x_zn = [40, 40.2]
 y_zn = [358, 769 ]
@@ -214,9 +217,10 @@ for name, value, uncertainty in zip('ab', params, uncertainties):
     print(f'{name} = {value:8.3f} ± {uncertainty:.3f}')
 
 t_zn = (471 + 81842)/2055
-t_zn1 = ufloat(t_ga, 0.05)
-print('t_zn =', t_zn)
-print('E_kZn = ', E(t_zn1/2))
+t_zn1 = ufloat(t_zn, 0.05)
+#print('t_zn =', t_zn)
+#print('E_kZn = ', E(t_zn1/2))
+E_kZn =  E(t_zn1/2)
 
 plt.plot(np.linspace(40,40.2,50),sigmoid(np.linspace(40, 40.2, 50), 2055, -81842), color ='green', label=r'Hilfsgerade')
 plt.plot(t_zn, I_kZn, 'rx', label=r'$I_K$')
@@ -230,7 +234,7 @@ plt.savefig('build/AbsorberZn.pdf')
 plt.clf()
 
 #### Strontium Sr #####
-print('----. Strontium ----------')
+#print('----. Strontium ----------')
 datenSr = np.loadtxt('AbsorberSr.csv', delimiter=',', skiprows=1)
 
 theta = datenSr[:,0]
@@ -240,9 +244,9 @@ I_maxSr = np.max(I_Sr)
 I_minSr = np.min(I_Sr)
 I_kSr = np.min(I_Sr)+(np.max(I_Sr)-np.min(I_Sr))/2
 
-print('I_minSr = ',np.min(I_Sr))
-print('I_maxSr = ',np.max(I_Sr))
-print('I_kSr = ',I_kSr)
+#print('I_minSr = ',np.min(I_Sr))
+#print('I_maxSr = ',np.max(I_Sr))
+#print('I_kSr = ',I_kSr)
 
 x_sr = [22, 22.2]
 y_sr = [65, 83 ]
@@ -257,8 +261,9 @@ for name, value, uncertainty in zip('ab', params, uncertainties):
 
 t_sr = (72 + 1915)/90
 t_sr1 = ufloat(t_sr, 0.05)
-print('t_sr =', t_sr)
-print('E_kSr = ', E(t_sr1/2))
+#print('t_sr =', t_sr)
+#print('E_kSr = ', E(t_sr1/2))
+E_kSr =  E(t_sr1/2)
 
 plt.plot(np.linspace(22,22.2,50),sigmoid(np.linspace(22, 22.2, 50), 90, -1915), color ='green', label=r'Hilfsgerade')
 plt.plot(t_sr, I_kSr, 'rx', label=r'$I_K$')
@@ -271,4 +276,40 @@ plt.savefig('build/AbsorberSr.pdf')
 
 plt.clf()
 
-#def sigmak(t):
+alpha = 1/137
+#print('------ Abschirmkonstante----------')
+
+s_kBr = (E(t_br1/2))/(R) - (alpha**2 * 35**4)/(4)
+s_kGa =(E(t_ga1/2))/(R) - (alpha**2 * 31**4)/(4)
+s_kZn =(E(t_zn1/2))/(R) - (alpha**2 * 30**4)/(4)
+s_kSr =(E(t_sr1/2))/(R) - (alpha**2 * 38**4)/(4)
+
+#print('s_kBr', 35-unp.sqrt(s_kBr))
+#print('s_kGa', 31-unp.sqrt(s_kGa))
+#print('s_kZn', 30-unp.sqrt(s_kZn))
+#print('s_kSr', 38-unp.sqrt(s_kSr))
+
+Z = [30, 31, 35, 38 ]
+E = [8986, 10396, 13442, 16070]
+
+from scipy.optimize import curve_fit
+params, covariance_matrix = curve_fit(sigmoid, np.sqrt(E), Z)
+
+uncertainties = np.sqrt(np.diag(covariance_matrix))
+
+for name, value, uncertainty in zip('ab', params, uncertainties): 
+    print(f'{name} = {value:8.3f} ± {uncertainty:.3f}')
+# a = 0.258 +-0,018  ; b = 5.202+- 1.965
+
+asf = np.linspace(90, 140, 50)
+
+plt.plot (np.sqrt(E), Z, 'rx', label=r'$Daten$')
+plt.plot(asf, sigmoid(asf, 0.258, 5.202), label=r'Ausgleichsgerade')
+plt.grid(True)
+plt.xlabel(r'$\sqrt{E_{K}}$')
+plt.ylabel(r'Ordnungszahl Z')
+plt.savefig('build/Moseley.pdf')
+
+d = ufloat(0.257578,0.018)
+R = 1/d**2
+print('R= ', R)
