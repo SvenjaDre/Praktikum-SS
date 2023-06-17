5d7e9e5d39f2a962600ea35d2673d4e9fa12e157
Svenja Dreyer <svenja.dreyer@tu-dortmund.de>
2023-06-17 10:22:55 +0200

    Auswertung 1/3

--- V601/vXXX/plot.py
+++ V601/vXXX/plot.py
@@ -3,17 +3,20 @@ import numpy as np
 from uncertainties import ufloat
 import uncertainties.unumpy as unp 
 ######## P und w Rechnung #####
-
+a = 0.01
 def p(T):
     return 5.5*10**7*unp.exp(-6876/(T+273.15))
 
-t = np.array([27.7, 148, 196, 198.4, 170, 173, 161, 154])
-T = unp.uarray(t, 1)
-
-print(p(T))
+t = np.array([27.7, 148, 154, 161, 170, 196 ])  #173,, 198.4
+T = unp.uarray(t, 2)
+#print(t+273.15)
+print('Sättigungsdruck')
+#print(p(T))
 
 def w(p_T):
     return 0.0029/p_T
 
 print('Wellenlänge')
-print(w(p(T)))
+#print(w(p(T)))
+v = a/w(p(T)*10**2)
+print(v)
